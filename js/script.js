0cd35f0cf16f08eb0caab88d5c9e85ba80a59b10
Valerio <valerio.modesti@gmail.com>
2020-07-06 15:25:55 +0200

    js-pwdegen-wannabe

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var name = prompt('Inserisci il tuo nome');
+var surname = prompt('Inserisci il tuo cognome');
+var color = prompt('Inserisci il tuo colore preferito');
+var number = 19;
+document.getElementById('pwd-gen').innerHTML = name + surname + color + number;
